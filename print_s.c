a4f1ffa62f385e057a643df9c67049720d753e42
Theo DUVAL <tduval@e3r9p15.42.fr>
2018-11-28 15:29:17 +0100

    42FC 15 fails

--- print_s.c
+++ print_s.c
@@ -6,7 +6,7 @@
 /*   By: tduval <tduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 21:45:30 by tduval            #+#    #+#             */
-/*   Updated: 2018/11/28 02:54:18 by tduval           ###   ########.fr       */
+/*   Updated: 2018/11/28 15:28:36 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,29 @@ int		print_s(va_list ap, t_flags elem)
 	s = va_arg(ap, char *);
 	if (!s)
 	{
-		ft_putstr("(null)");
-		return (6);
+		free(s);
+		s = "(null)";
 	}
-	while (elem.width && (size_t)(elem.width - i) > ft_strlen(s) && !(ft_strchr(elem.options, '-')))
+	j = ft_strlen(s) > elem.accuracy ? elem.accuracy : ft_strlen(s);
+	while (elem.width && (size_t)(elem.width - i) > j && !(ft_strchr(elem.options, '-')))
 	{
 		ft_putchar(' ');
 		i++;
 	}
+	j = 0;
 	if ((int)ft_strlen(s) > elem.accuracy && elem.accuracy != -1)
 	{
 		while (j < elem.accuracy && s[j])
 		{
 			ft_putchar(s[j]);
-			i ++;
+			i++;
 			j++;
 		}
 	}
 	else
 		i += (int)write(1, s, ft_strlen((char *)s));
-	while (elem.width && (size_t)elem.width > ft_strlen(s) && ft_strchr(elem.options, '-'))
+	j = ft_strlen(s) > elem.accuracy ? elem.accuracy : ft_strlen(s);
+	while (elem.width && (size_t)elem.width > j && ft_strchr(elem.options, '-'))
 	{
 		ft_putchar(' ');
 		elem.width--;
